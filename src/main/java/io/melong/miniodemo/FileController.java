d41b6320f31fd5bc1b2bd3ab37164f3a252bb9e5
neoty <neoty@naver.com>
2021-09-28 22:54:54 +0900

    test

--- src/main/java/io/melong/miniodemo/FileController.java
+++ src/main/java/io/melong/miniodemo/FileController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -20,22 +21,36 @@ import java.util.*;
 @Slf4j
 @Controller
 public class FileController {
-
-
-
     private MinioClient minioClient;
     private static final String BUCKET_NAME = "test";
     @GetMapping("/")
     public String form(Model model) throws Exception {
+        Iterable<Result<Item>> results = minioClient.listObjects(ListObjectsArgs.builder()
+                        .bucket(BUCKET_NAME)
+                .build());
+
+        ArrayList<String> objectNames = new ArrayList<>();
+        for (Result<Item> result : results) {
+            objectNames.add(result.get().objectName());
+        }
+        model.addAttribute("objectNames", objectNames);
+
         return "uploadForm";
     }
 
     @PostMapping("/")
-    public String upload(@RequestParam("file") MultipartFile multipartFile) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+    public String upload(@RequestParam("file") MultipartFile multipartFile,
+                         RedirectAttributes redirectAttributes
+    ) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
 
         if (! minioClient.bucketExists(BucketExistsArgs.builder().bucket(BUCKET_NAME).build())) {
             minioClient.makeBucket(MakeBucketArgs.builder().bucket(BUCKET_NAME).build());
             log.info("bucket created");
+        }
+
+        if (multipartFile.getSize() < 1) {
+            redirectAttributes.addFlashAttribute("message", "is correct file?");
+            return "redirect:/";
         }
 
         Map<String, String> tags = new HashMap<>();
@@ -48,22 +63,16 @@ public class FileController {
                         .contentType(multipartFile.getContentType())
                         .tags(tags)
                 .build());
-        log.info("minio upload success");
 
+        redirectAttributes.addFlashAttribute("message", "upload success");
         return "redirect:/";
-    }
-
-    private void getMinioClient() {
-        if (minioClient == null) {
-            minioClient = MinioClient.builder()
-                    .endpoint("http://localhost", 9000, false)
-                    .credentials("minio", "minio123")
-                    .build();
-        }
     }
 
     @PostConstruct
     void init() {
-        getMinioClient();
+        minioClient = MinioClient.builder()
+                .endpoint("http://localhost", 9000, false)
+                .credentials("minio", "minio123")
+                .build();
     }
 }
