cffb053cf01db30d03e2db6f069767c50f149b61
neoty <neoty@naver.com>
2021-09-30 11:07:18 +0900

    complete

--- src/main/java/io/melong/miniodemo/FileController.java
+++ src/main/java/io/melong/miniodemo/FileController.java
@@ -4,9 +4,13 @@ import io.minio.*;
 import io.minio.errors.*;
 import io.minio.messages.Item;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +18,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
@@ -53,19 +60,44 @@ public class FileController {
             return "redirect:/";
         }
 
-        Map<String, String> tags = new HashMap<>();
-        tags.put("fileName", multipartFile.getOriginalFilename());
+        Map<String, String> metas = new HashMap<>();
+        metas.put("name", multipartFile.getOriginalFilename());
 
         minioClient.putObject(PutObjectArgs.builder()
                         .bucket(BUCKET_NAME)
                         .object(UUID.randomUUID().toString())
                         .stream(multipartFile.getInputStream(), multipartFile.getSize(), -1)
                         .contentType(multipartFile.getContentType())
-                        .tags(tags)
+                        .userMetadata(metas)
                 .build());
 
         redirectAttributes.addFlashAttribute("message", "upload success");
         return "redirect:/";
+    }
+
+    @GetMapping("/{objectId}")
+    public ResponseEntity<InputStreamResource> download(@PathVariable("objectId") String objectId,
+                                                        RedirectAttributes redirectAttributes) {
+        try {
+            InputStream stream = minioClient.getObject(GetObjectArgs.builder()
+                    .bucket(BUCKET_NAME)
+                    .object(objectId)
+                    .build());
+            StatObjectResponse statObjectResponse = minioClient.statObject(StatObjectArgs.builder()
+                    .bucket(BUCKET_NAME)
+                    .object(objectId)
+                    .build());
+            Map<String, String> metas = statObjectResponse.userMetadata();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_TYPE, statObjectResponse.contentType());
+            headers.add("Content-Disposition",
+                    "attachment; filename=\""+ URLEncoder.encode(metas.get("name"), StandardCharsets.UTF_8).replaceAll("\\+", "%20")+"\"");
+            return ResponseEntity.ok().headers(headers).body(new InputStreamResource(stream));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @PostConstruct
